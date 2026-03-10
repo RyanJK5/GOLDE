af721d3fd8dfd7fffb6cec8bbb637fecc8e9fac4
Ryan <ryan.keane051@gmail.com>
2026-03-10 09:04:22 -0400

    [BUGFIX] Fixed linker error

--- GOLGuiLib/include/SimulationCommand.hpp
+++ GOLGuiLib/include/SimulationCommand.hpp
@@ -64,9 +64,55 @@ using SimulationCommand =
 
 // Convert individual action enum values to SimulationCommand.
 // Used by Widget::UpdateResult for simple (no-payload) buttons.
-SimulationCommand ToCommand(GameAction action);
-SimulationCommand ToCommand(EditorAction action);
-SimulationCommand ToCommand(SelectionAction action);
+inline SimulationCommand ToCommand(GameAction action) {
+    switch (action) {
+        using enum GameAction;
+    case Start:
+        return StartCommand{};
+    case Pause:
+        return PauseCommand{};
+    case Resume:
+        return ResumeCommand{};
+    case Restart:
+        return RestartCommand{};
+    case Reset:
+        return ResetCommand{};
+    case Clear:
+        return ClearCommand{};
+    case Step:
+        return StepCommand{};
+    }
+    std::unreachable();
+}
+
+inline SimulationCommand ToCommand(EditorAction action) {
+    switch (action) {
+        using enum EditorAction;
+    case Resize:
+        return ResizeCommand{};
+    case GenerateNoise:
+        return GenerateNoiseCommand{};
+    case Undo:
+        return UndoCommand{};
+    case Redo:
+        return RedoCommand{};
+    case Save:
+        return SaveCommand{};
+    case SaveAsNew:
+        return SaveAsNewCommand{};
+    case NewFile:
+        return NewFileCommand{};
+    case Load:
+        return LoadCommand{};
+    case Close:
+        return CloseCommand{};
+    }
+    std::unreachable();
+}
+
+inline SimulationCommand ToCommand(SelectionAction action) {
+    return SelectionCommand{action};
+}
 
 } // namespace gol
 
