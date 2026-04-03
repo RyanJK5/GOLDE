a8856174a5fe8ab7eb3e74446f3a1fbc967ffa08
Ryan <ryan.keane051@gmail.com>
2026-04-03 10:46:16 -0400

    [REFACTOR] Output executable is now named GOLDE

---
+++ GOLExecutable/src/Application.cpp
@@ -0,0 +1,13 @@
+#include <filesystem>
+
+#include "Game.hpp"
+
+int main(int argc, char* argv[]) {
+    if (argc > 0) {
+        const auto executablePath = std::filesystem::absolute(argv[0]);
+        std::filesystem::current_path(executablePath.parent_path());
+    }
+
+    gol::Game game{};
+    game.Begin();
+}
